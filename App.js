72eacc8a39148d5afd44f070296c0b64c20d81c5
Cezarijus Kivylius <Cezarijus.Kivyliys@hertz.com>
2022-10-04 21:56:51 +0100

    test commit

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { registerDevMenuItems } from "expo-dev-client";
+
+const devMenuItems = [
+  {
+    name: "My Custom Button",
+    callback: () => console.log("Hello world!"),
+  },
+];
+
+registerDevMenuItems(devMenuItems);
+
 
 export default function App() {
   return (
